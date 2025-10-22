100b0f6300e4ddb3667ed041d72dc31d7b1914c2
Ibrahim Ahmed Galib <galib@Ibrahims-Mac-mini.local>
2025-10-22 10:20:18 +0600

    Pass id using useParms in categoryNews.jsx

--- src/pages/CategoryNews.jsx
+++ src/pages/CategoryNews.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useParams } from 'react-router';
 
 const CategoryNews = () => {
+    const {id}=useParams();
+    
     return (
         <div>
-            Category News
+            Category News-{id}
         </div>
     );
 };
