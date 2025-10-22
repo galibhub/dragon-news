8fbeb779c1f490821e24494f56f7a1764d3998e3
Ibrahim Ahmed Galib <galib@Ibrahims-Mac-mini.local>
2025-10-22 14:19:10 +0600

    Load news by category: All news,Breaking...

--- src/pages/CategoryNews.jsx
+++ src/pages/CategoryNews.jsx
@@ -1,14 +1,31 @@
-import React from 'react';
-import { useParams } from 'react-router';
+import React, { useEffect, useState } from "react";
+import { useLoaderData, useParams } from "react-router";
 
 const CategoryNews = () => {
-    const {id}=useParams();
-    
-    return (
-        <div>
-            Category News-{id}
-        </div>
-    );
+  const { id } = useParams(); // id - string
+  const data = useLoaderData();
+
+  const [categoryNews, setCategoryNews] = useState([]);
+
+  useEffect(() => {
+    if (id == "0") {
+      setCategoryNews(data);
+      return;
+    }
+     else if (id == "1") {
+      const filteredNews = data.filter(
+        (news) => news.others.is_today_pick == true
+      );
+      setCategoryNews(filteredNews);
+    } 
+    else {
+      const filteredNews = data.filter((news) => news.category_id == id);
+      console.log(filteredNews);
+      setCategoryNews(filteredNews);
+    }
+  }, [data, id]); // data and id cames from outside-dependency
+
+  return <div>total {categoryNews.length} news found</div>;
 };
 
 export default CategoryNews;
